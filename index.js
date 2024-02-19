5cfb2a4781170d4cb3db8bddc0a5f43343224972
KenniBlank <keniblank@gmail.com>
2024-02-19 10:09:02 -0800

    added flippying option to the question which will be provided

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
+// this has the simple job of flipping the graph question and the user's interpretation
+document.getElementById("graphs").addEventListener('click', () => {
+    const problem = document.getElementById('problem');
+    const usrSolution = document.getElementById('usr_solution');
 
-document.getElementById('button').addEventListener('click', plotGraph());
-
-
-function plotGraph() {
-    let equation = document.getElementById('equationInput').value;
-    let match= equation.match(/y\s*=\s*(\d+)\s*x\s*\+\s*(\d+)/);
-
-    let m = parseInt(match[1]);
-    console.log(m);
-    let b = parseInt(match[2]);
-    console.log(b);
-
-    let xValues = [];
-    let yValues = [];
-
-    for (let x = -100; x <= 100; x++) {
-        xValues.push(x);
-        yValues.push(m * x + b);
+    if (problem.classList.contains('flipped')) {
+        problem.style.transform = 'rotateX(0deg)';
+        usrSolution.style.transform = 'rotateX(-180deg)';
+        problem.classList.remove('flipped');
+    } else {
+        problem.style.transform = 'rotateX(180deg)';
+        usrSolution.style.transform = 'rotateX(0deg)';
+        problem.classList.add('flipped');
     }
+});
 
-    let data = [{
-        x: xValues,
-        y: yValues,
-        type: 'scatter',
-        mode: 'lines',
-        name: 'y = ' + m + 'x + ' + b
-    }];
 
-    Plotly.newPlot('graph', data, {title: 'Graph Plotter'});}
+// document.getElementById('button').addEventListener('click', plotGraph());
+
+
+// function plotGraph() {
+//     let equation = document.getElementById('equationInput').value;
+//     let match= equation.match(/y\s*=\s*(\d+)\s*x\s*\+\s*(\d+)/);
+
+//     let m = parseInt(match[1]);
+//     console.log(m);
+//     let b = parseInt(match[2]);
+//     console.log(b);
+
+//     let xValues = [];
+//     let yValues = [];
+
+//     for (let x = -100; x <= 100; x++) {
+//         xValues.push(x);
+//         yValues.push(m * x + b);
+//     }
+
+//     let data = [{
+//         x: xValues,
+//         y: yValues,
+//         type: 'scatter',
+//         mode: 'lines',
+//         name: 'y = ' + m + 'x + ' + b
+//     }];
+
+//     Plotly.newPlot('graph', data, {title: 'Graph Plotter'});}
