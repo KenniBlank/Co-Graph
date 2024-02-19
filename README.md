4a3c43e97a6b4df38dff07c04e57a1415edd23e4
KenniBlank <keniblank@gmail.com>
2024-02-19 14:53:49 -0800

    Some major changes, individual ones

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Co-Graph
-...
+
+New changes
+- Added card flip animation to the problem and the user's answer
+- made stable graph for randomness
+- errors with handling the checking part of the graph
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // this has the simple job of flipping the graph question and the user's interpretation
+// No changes needed
 document.getElementById("graphs").addEventListener('click', () => {
     const problem = document.getElementById('problem');
     const usrSolution = document.getElementById('usr_solution');
@@ -15,32 +16,3 @@ document.getElementById("graphs").addEventListener('click', () => {
 });
 
 
-// document.getElementById('button').addEventListener('click', plotGraph());
-
-
-// function plotGraph() {
-//     let equation = document.getElementById('equationInput').value;
-//     let match= equation.match(/y\s*=\s*(\d+)\s*x\s*\+\s*(\d+)/);
-
-//     let m = parseInt(match[1]);
-//     console.log(m);
-//     let b = parseInt(match[2]);
-//     console.log(b);
-
-//     let xValues = [];
-//     let yValues = [];
-
-//     for (let x = -100; x <= 100; x++) {
-//         xValues.push(x);
-//         yValues.push(m * x + b);
-//     }
-
-//     let data = [{
-//         x: xValues,
-//         y: yValues,
-//         type: 'scatter',
-//         mode: 'lines',
-//         name: 'y = ' + m + 'x + ' + b
-//     }];
-
-//     Plotly.newPlot('graph', data, {title: 'Graph Plotter'});}
